24fa8bb03adb002bd5a150e94af770e37c4f1c40
ehmilioo <59847442+ehmilioo@users.noreply.github.com>
2021-12-01 18:47:38 +0100

    Added counter

--- day1/day1.js
+++ day1/day1.js
@@ -1,3 +1,4 @@
+const time = Date.now();
 const fs = require('fs');
 const read = fs.readFileSync("day1.txt");
 let lines = read.toString().split("\n").map(Number);
@@ -18,3 +19,5 @@ for (var i=1; i<lines.length; i++) {
 
 console.log(`First Answer: ${firstAnswer}`);
 console.log(`Second Answer: ${secondAnswer}`);
+console.log(`Time lapsed: ${(Date.now() - time)/1000}s`)
+// Between 0.005 - 0.006s
