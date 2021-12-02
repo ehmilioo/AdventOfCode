fab01e0b84ca3348f77db715d998279e61103944
emilkrslak <krslak_emil@hotmail.com>
2021-12-02 11:38:37 +0100

    Optimized for-loops

--- day2/day2.js
+++ day2/day2.js
@@ -1,33 +1,44 @@
-const time = Date.now();
+let t0 = Date.now();
 const fs = require('fs');
 const read = fs.readFileSync("day2.txt");
 let lines = read.toString().split("\n");
 let x = 0;
 let y = 0;
-let y2 = 0;
-let aim = 0;
 
-for(var i=0; i<lines.length; i++){
-  let dirr = lines[i].split(' ')[0];
-  let amount = parseInt(lines[i].split(' ')[1]);
-  switch(dirr){
-    case 'forward':
-      x+=amount;
-      y2+=aim*amount;
-      break;
-    case 'down':
-      aim+=amount;
-      y+=amount;
-      break
-    case 'up':
-      aim-=amount;
-      y-=amount;
-      break;
+for(command of lines){
+  let direction = command.split(' ')[0];
+  let amount = parseInt(command.split(' ')[1]);
+  switch(direction){
+    case 'forward': x+=amount; break
+    case 'down': y+=amount; break
+    case 'up': y-=amount; break
   }
     
 }
 
+//Answer: 1989014
 console.log(`First Answer: ${x*y}`);
-console.log(`Second Answer: ${x*y2}`);
-console.log(`Time lapsed: ${(Date.now() - time)/1000}s`)
-// Between 0.042 - 0.043s
+console.log(`P1 Time: ${(Date.now() - t0)/1000}s`)
+t1 = Date.now();
+// Between 0.009 - 0.01s
+
+x = 0;
+y = 0;
+let aim = 0;
+
+for(command of lines){
+  let direction = command.split(' ')[0];
+  let amount = parseInt(command.split(' ')[1]);
+  switch(direction){
+    case 'down': aim+= +amount; break
+    case 'forward':
+      x+= +amount;
+      y+= +amount*aim;
+      break;
+  }
+}
+
+//Answer: 2006917119
+console.log(`Second Answer: ${x*y}`);
+console.log(`P2 Time: ${(Date.now() - t1)/1000}s`)
+// Between 0 - 0.001s
