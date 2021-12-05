da21e848bc579bf3d4101df0a3e0d436f619c0c0
ehmilioo <59847442+ehmilioo@users.noreply.github.com>
2021-12-05 15:14:49 +0100

    day5 cleanup?

--- day5/day5.js
+++ day5/day5.js
@@ -1,5 +1,4 @@
 console.time("Program");
-console.time("P1");
 const fs = require("fs");
 const read = fs.readFileSync("day5.txt");
 const lines = read.toString().split("\n").filter(line => line); //Fix bad line
@@ -9,8 +8,21 @@ let map = new Map();
 let secondMap = new Map();
 
 for (const vector of sorted) {
+    let [x, y] = vector[0];
+    let [x0, y0] = vector[1];
+
     let [x1, y1] = vector[0];
     let [x2, y2] = vector[1];
+
+    xPos = Math.sign(x0 - x);
+    yPos = Math.sign(y0 - y);
+    while(x != x0 + xPos || y != y0 + yPos){
+        let key = `${x},${y}`;
+        let value = (secondMap.get(key) ?? 0) + 1;
+        secondMap.set(key, value);
+        x += xPos;
+        y += yPos;
+    }
 
     if(x1 == x2 || y1 == y2){
         xPositive = Math.sign(x2 - x1);
@@ -24,36 +36,14 @@ for (const vector of sorted) {
         }
     }
 }
+
 let arr = Array.from(map.values())
 let filtered = arr.filter(num => num >= 2);
+console.log("\n");
 console.log(`P1 Answer: ${filtered.length}`)
-console.timeEnd("P1");
-console.time("P2");
-
-for (const vector of sorted) {
-    let [x1, y1] = vector[0];
-    let [x2, y2] = vector[1];
-    
-    xPositive = Math.sign(x2 - x1);
-    yPositive = Math.sign(y2 - y1);
-    while(x1 != x2 + xPositive || y1 != y2 + yPositive){
-        let key = `${x1},${y1}`;
-        let value = (secondMap.get(key) ?? 0) + 1;
-        secondMap.set(key, value);
-        x1 += xPositive;
-        y1 += yPositive;
-    }
-}
 
 arr = Array.from(secondMap.values())
 filtered = arr.filter(num => num >= 2);
 
 console.log(`P2 Answer: ${filtered.length}`)
 console.timeEnd("Program");
-console.timeEnd("P2");
-
-// P1 Answer: 6548
-// P1: 43.671ms
-// P2 Answer: 19663
-// Program: 108.242ms
-// P2: 64.648ms
